e10b2e2e146584e227bd50dc726c6dbda1cd8ada
Matthew Snaith <fault_line@hotmail.co.uk>
2014-08-14 14:54:03 +0100

    postcode form tests added to SetupTradeDetailsFormSpec

--- test/controllers/acquire/SetupTradeDetailsFormSpec.scala
+++ test/controllers/acquire/SetupTradeDetailsFormSpec.scala
@@ -27,7 +27,7 @@ class SetupTradeDetailsFormSpec extends UnitSpec {
       errors(2).key should equal(TraderNameId)
       errors(2).message should equal("error.validTraderBusinessName")
     }
-    
+
     "reject if trader business name is less than minimum length" in {
       formWithValidDefaults(traderBusinessName = "A").errors should have length 1
     }
@@ -39,6 +39,36 @@ class SetupTradeDetailsFormSpec extends UnitSpec {
     "accept if trader business name is valid" in {
       formWithValidDefaults(traderBusinessName = TraderBusinessNameValid, traderPostcode = PostcodeValid).
         get.traderBusinessName should equal(TraderBusinessNameValid.toUpperCase)
+    }
+  }
+
+  "postcode" should {
+    "reject if trader postcode is empty" in {
+      // IMPORTANT: The messages being returned by the form validation are overridden by the Controller
+      val errors = formWithValidDefaults(traderPostcode = "").errors
+      errors should have length 3
+      errors(0).key should equal(TraderPostcodeId)
+      errors(0).message should equal("error.minLength")
+      errors(1).key should equal(TraderPostcodeId)
+      errors(1).message should equal("error.required")
+      errors(2).key should equal(TraderPostcodeId)
+      errors(2).message should equal("error.restricted.validPostcode")
+    }
+
+    "reject if trader postcode is less than the minimum length" in {
+      formWithValidDefaults(traderPostcode = "M15A").errors should have length 2
+    }
+
+    "reject if trader postcode is more than the maximum length" in {
+      formWithValidDefaults(traderPostcode = "SA99 1DDD").errors should have length 2
+    }
+
+    "reject if trader postcode contains special characters" in {
+      formWithValidDefaults(traderPostcode = "SA99 1D$").errors should have length 1
+    }
+
+    "reject if trader postcode contains an incorrect format" in {
+      formWithValidDefaults(traderPostcode = "SAR99").errors should have length 1
     }
   }
 
