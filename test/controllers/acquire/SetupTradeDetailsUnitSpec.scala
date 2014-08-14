da560d175a2663ca1264b37ed7b4d6b0719a036c
Matthew Snaith <fault_line@hotmail.co.uk>
2014-08-14 13:16:22 +0100

    cookie tests added to SetupTradeDetailsUnitSpec

--- test/controllers/acquire/SetupTradeDetailsUnitSpec.scala
+++ test/controllers/acquire/SetupTradeDetailsUnitSpec.scala
@@ -4,6 +4,8 @@ import helpers.UnitSpec
 import controllers.SetUpTradeDetails
 import play.api.test.{WithApplication, FakeRequest}
 import play.api.test.Helpers._
+import helpers.acquire.CookieFactoryForUnitSpecs
+import pages.acquire.SetupTradeDetailsPage.{TraderBusinessNameValid, PostcodeValid}
 
 
 class SetupTradeDetailsUnitSpec extends UnitSpec {
@@ -14,6 +16,21 @@ class SetupTradeDetailsUnitSpec extends UnitSpec {
         r =>
           r.header.status should equal(OK)
       }
+    }
+
+    "display populated fields when cookie exists" in new WithApplication {
+      val request = FakeRequest().
+        withCookies(CookieFactoryForUnitSpecs.setupTradeDetails())
+      val result = setUpTradeDetails.present(request)
+      val content = contentAsString(result)
+      content should include(TraderBusinessNameValid)
+      content should include(PostcodeValid)
+    }
+
+    "display empty fields when cookie does not exist" in new WithApplication {
+      val content = contentAsString(present)
+      content should not include TraderBusinessNameValid
+      content should not include PostcodeValid
     }
   }
 
