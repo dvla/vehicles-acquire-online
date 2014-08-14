a836a83376c0933585eb9a7975ef2cba097b7f90
Matthew Snaith <fault_line@hotmail.co.uk>
2014-08-14 12:21:00 +0100

    further tests added to SetupTradeDetailsIntegrationSpec. added Accessibility and ErrorPanel files

--- test/views/acquire/SetupTradeDetailsIntegrationSpec.scala
+++ test/views/acquire/SetupTradeDetailsIntegrationSpec.scala
@@ -4,12 +4,8 @@ import helpers.UiSpec
 import helpers.common.ProgressBar.progressStep
 import helpers.tags.UiTag
 import helpers.webbrowser.TestHarness
-import org.openqa.selenium.{By, WebElement}
-//import pages.common.Accessibility
-//import pages.common.ErrorPanel
-//import pages.common.{Accessibility, ErrorPanel}
+import pages.common.{Accessibility, ErrorPanel}
 import pages.acquire.SetupTradeDetailsPage._
-import pages.acquire.SetupTradeDetailsPage
 import pages.acquire.SetupTradeDetailsPage
 import uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction
 import viewmodels.SetupTradeDetailsViewModel
@@ -28,6 +24,41 @@ final class SetupTradeDetailsIntegrationSpec extends UiSpec with TestHarness {
       happyPath()
 
       page.title should equal("Success")
+    }
+
+    "display one summary validation error message when no postcode is entered" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessName = TraderBusinessNameValid, traderBusinessPostcode = "")
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display one summary validation error message when no trader business name is entered" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessName = "", traderBusinessPostcode = PostcodeValid)
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display two summary validation error messages when no details are entered" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessName = "", traderBusinessPostcode = "")
+      ErrorPanel.numberOfErrors should equal(2)
+    }
+
+    "add aria required attribute to trader name field when required field not input" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessName = "")
+      Accessibility.ariaRequiredPresent(SetupTradeDetailsViewModel.Form.TraderNameId) should equal(true)
+    }
+
+    "add aria invalid attribute to trader name field when required field not input" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessName = "")
+      Accessibility.ariaInvalidPresent(SetupTradeDetailsViewModel.Form.TraderNameId) should equal(true)
+    }
+
+    "add aria required attribute to trader postcode field when required field not input" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessPostcode = "")
+      Accessibility.ariaRequiredPresent(SetupTradeDetailsViewModel.Form.TraderPostcodeId) should equal(true)
+    }
+
+    "add aria invalid attribute to trader postcode field when required field not input" taggedAs UiTag in new WebBrowser {
+      happyPath(traderBusinessPostcode = "")
+      Accessibility.ariaInvalidPresent(SetupTradeDetailsViewModel.Form.TraderPostcodeId) should equal(true)
     }
   }
 
