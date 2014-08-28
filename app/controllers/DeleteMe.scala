e5380ecabf93dbceca2539040dae91b16b8e5fd2
Ian Stainer <ian.stainer@valtech.co.uk>
2014-08-28 11:52:09 +0100

    Building out the keeper still on record page

--- app/controllers/DeleteMe.scala
+++ app/controllers/DeleteMe.scala
@@ -4,12 +4,14 @@ import com.google.inject.Inject
 import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
 import utils.helpers.Config
+import uk.gov.dvla.vehicles.presentation.common.model.VehicleDetailsModel
 
 final class DeleteMe @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                        config: Config) extends Controller {
 
   def present = Action { implicit request =>
-    Ok(views.html.acquire.keeper_still_on_record())
+    val data = VehicleDetailsModel(registrationNumber = "BD54 XKF", vehicleMake = "Peugeot", vehicleModel = "307")
+    Ok(views.html.acquire.keeper_still_on_record(data))
   }
 
 }
