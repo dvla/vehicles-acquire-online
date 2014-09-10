04165e23d289f997c095cee82c25d223bbebe71d
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-09-10 16:02:31 +0100

    Using the new and improced date of birth mapping

--- app/viewmodels/PrivateKeeperDetailsCompleteFormModel.scala
+++ app/viewmodels/PrivateKeeperDetailsCompleteFormModel.scala
@@ -1,15 +1,15 @@
 package viewmodels
 
+import org.joda.time.LocalDate
 import play.api.data.Forms.mapping
 import play.api.libs.json.Json
 import uk.gov.dvla.vehicles.presentation.common.clientsidesession.CacheKey
-import uk.gov.dvla.vehicles.presentation.common.mappings.DateOfBirth.optionalDateOfBirth
-import uk.gov.dvla.vehicles.presentation.common.views.models.DateOfBirth
+import uk.gov.dvla.vehicles.presentation.common.mappings.DateOfBirth.optionalMapping
 
-case class PrivateKeeperDetailsCompleteFormModel(dateOfBirth: Option[DateOfBirth])
+case class PrivateKeeperDetailsCompleteFormModel(dateOfBirth: Option[LocalDate])
 
 object PrivateKeeperDetailsCompleteFormModel {
-  implicit val DateOfBirthJsonFormat = Json.format[DateOfBirth]
+//  implicit val DateOfBirthJsonFormat = Json.format[DateOfBirth]
   implicit val JsonFormat = Json.format[PrivateKeeperDetailsCompleteFormModel]
   final val PrivateKeeperDetailsCompleteCacheKey = "privateKeeperDetailsComplete"
   implicit val Key = CacheKey[PrivateKeeperDetailsCompleteFormModel](PrivateKeeperDetailsCompleteCacheKey)
@@ -18,7 +18,7 @@ object PrivateKeeperDetailsCompleteFormModel {
     final val DateOfBirthId = "privatekeeper_dateofbirth"
 
     final val Mapping = mapping(
-      DateOfBirthId -> optionalDateOfBirth
+      DateOfBirthId -> optionalMapping
     )(PrivateKeeperDetailsCompleteFormModel.apply)(PrivateKeeperDetailsCompleteFormModel.unapply)
   }
 }
