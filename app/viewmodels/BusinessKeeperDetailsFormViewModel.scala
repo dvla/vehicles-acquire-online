c5f886a9fe3b63c6a37c4403f5fd42538ea6ac06
Ian Stainer <ian.stainer@valtech.co.uk>
2014-09-10 16:29:36 +0100

    Added email address to business-keeper-details

--- app/viewmodels/BusinessKeeperDetailsFormViewModel.scala
+++ app/viewmodels/BusinessKeeperDetailsFormViewModel.scala
@@ -1,13 +1,14 @@
 package viewmodels
 
 import mappings.FleetNumber.fleetNumberMapping
-import play.api.data.Forms.mapping
+import play.api.data.Forms._
 import play.api.libs.json.Json
 import uk.gov.dvla.vehicles.presentation.common
 import common.clientsidesession.CacheKey
 import common.mappings.BusinessName.businessNameMapping
+import common.mappings.Email.email
 
-final case class BusinessKeeperDetailsFormViewModel(fleetNumber: Option[String], businessName: String)
+final case class BusinessKeeperDetailsFormViewModel(fleetNumber: Option[String], businessName: String, email: Option[String])
 
 object BusinessKeeperDetailsFormViewModel {
   implicit val JsonFormat = Json.format[BusinessKeeperDetailsFormViewModel]
@@ -17,10 +18,12 @@ object BusinessKeeperDetailsFormViewModel {
   object Form {
     final val FleetNumberId = "fleetNumber"
     final val BusinessNameId = "businessName"
+    final val EmailId = "email"
 
     final val Mapping = mapping(
       FleetNumberId -> fleetNumberMapping,
-      BusinessNameId -> businessNameMapping
+      BusinessNameId -> businessNameMapping,
+      EmailId -> optional(email)
     )(BusinessKeeperDetailsFormViewModel.apply)(BusinessKeeperDetailsFormViewModel.unapply)
   }
 }
