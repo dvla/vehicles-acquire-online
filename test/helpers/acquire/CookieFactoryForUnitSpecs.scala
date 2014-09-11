752c520cd8e7021fb2f358a92fc3779a1a669541
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-09-11 08:59:58 +0100

    Verify the Date of birth controller works

--- test/helpers/acquire/CookieFactoryForUnitSpecs.scala
+++ test/helpers/acquire/CookieFactoryForUnitSpecs.scala
@@ -1,6 +1,7 @@
 package helpers.acquire
 
 import composition.TestComposition
+import org.joda.time.LocalDate
 import play.api.libs.json.{Json, Writes}
 import play.api.mvc.Cookie
 import uk.gov.dvla.vehicles.presentation.common
@@ -124,6 +125,11 @@ object CookieFactoryForUnitSpecs extends TestComposition { // TODO can we make t
       email = email
     )
     createCookie(key, value)
+  }
+
+  def privateKeeperDetailsCompleteModel(dateOfBirth: Option[LocalDate]): Cookie = {
+    val value = PrivateKeeperDetailsCompleteFormModel(dateOfBirth)
+    createCookie(PrivateKeeperDetailsCompleteFormModel.PrivateKeeperDetailsCompleteCacheKey, value)
   }
 
   def trackingIdModel(value: String = TrackingIdValue): Cookie = {
