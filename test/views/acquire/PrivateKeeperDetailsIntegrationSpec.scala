1d81135f58045d9dd199ef624976373cc7c3caaa
Ian Stainer <ian.stainer@valtech.co.uk>
2014-09-11 15:59:02 +0100

    Corrected and tidy up of private keeper integration spec

--- test/views/acquire/PrivateKeeperDetailsIntegrationSpec.scala
+++ test/views/acquire/PrivateKeeperDetailsIntegrationSpec.scala
@@ -24,38 +24,31 @@ final class PrivateKeeperDetailsIntegrationSpec extends UiSpec with TestHarness 
     "display the page" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       go to PrivateKeeperDetailsPage
-
       page.title should equal(PrivateKeeperDetailsPage.title)
     }
 
     "display the progress of the page when progressBar is set to true" taggedAs UiTag in new ProgressBarTrue {
       go to BeforeYouStartPage
       cacheSetup()
-
       go to PrivateKeeperDetailsPage
-
       page.source.contains(progressStep(5)) should equal(true)
     }
 
     "not display the progress of the page when progressBar is set to false" taggedAs UiTag in new ProgressBarFalse {
       go to BeforeYouStartPage
       cacheSetup()
-
       go to PrivateKeeperDetailsPage
-
       page.source.contains(progressStep(5)) should equal(false)
     }
 
     "Redirect when no vehicle details are cached" taggedAs UiTag in new WebBrowser {
-      go to VehicleLookupPage
-
+      go to PrivateKeeperDetailsPage
       page.title should equal(SetupTradeDetailsPage.title)
     }
 
     "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowser {
-      go to VehicleLookupPage
+      go to PrivateKeeperDetailsPage
       val csrf: WebElement = webDriver.findElement(By.name(CsrfPreventionAction.TokenName))
       csrf.getAttribute("type") should equal("hidden")
       csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
@@ -67,58 +60,45 @@ final class PrivateKeeperDetailsIntegrationSpec extends UiSpec with TestHarness 
     "go to the appropriate next page when all private keeper details are entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       happyPath()
-
       page.title should equal("Not implemented") //ToDo amend title once next page is implemented
     }
 
     "go to the appropriate next page when mandatory private keeper details are entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       happyPath(email = "")
-
       page.title should equal("Not implemented") //ToDo amend title once next page is implemented
     }
 
     "display one validation error message when no title is entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       sadPath(email = EmailValid, firstName = FirstNameValid, surname = SurnameValid)
-
       ErrorPanel.numberOfErrors should equal(1)
     }
 
     "display one validation error message when an incorrect email is entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       sadPath(title = OptionValid, firstName = FirstNameValid, surname = SurnameValid, email = EmailInvalid)
-
       ErrorPanel.numberOfErrors should equal(1)
     }
 
     "display one validation error message when no firstname is entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       sadPath(title = OptionValid, firstName = FirstNameInvalid, surname = SurnameValid, email = EmailInvalid)
-
       ErrorPanel.numberOfErrors should equal(2)
     }
 
     "display one validation error message when no surname is entered" taggedAs UiTag in new WebBrowser {
       go to BeforeYouStartPage
       cacheSetup()
-
       sadPath(title = OptionValid, firstName = SurnameInvalid, surname = SurnameValid, email = EmailInvalid)
-
       ErrorPanel.numberOfErrors should equal(2)
     }
   }
-
 
   "back" should {
     "display previous page when back link is clicked with uprn present" taggedAs UiTag in new WebBrowser {
@@ -129,9 +109,7 @@ final class PrivateKeeperDetailsIntegrationSpec extends UiSpec with TestHarness 
         vehicleDetails()
 
       go to PrivateKeeperDetailsPage
-
       click on back
-
       page.title should equal(VehicleLookupPage.title)
     }
   }
