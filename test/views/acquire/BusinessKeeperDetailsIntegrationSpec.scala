91b1c38df253e274c34c9e3283f7313e8b83f8da
Ian Stainer <ian.stainer@valtech.co.uk>
2014-09-11 15:44:59 +0100

    Added integration spec for business keeper

---
+++ test/views/acquire/BusinessKeeperDetailsIntegrationSpec.scala
@@ -0,0 +1,93 @@
+package views.acquire
+
+import helpers.common.ProgressBar
+import helpers.acquire.CookieFactoryForUISpecs
+import ProgressBar.progressStep
+import helpers.tags.UiTag
+import helpers.UiSpec
+import helpers.webbrowser.TestHarness
+import org.openqa.selenium.{By, WebElement, WebDriver}
+import pages.common.ErrorPanel
+import pages.acquire.BeforeYouStartPage
+import pages.acquire.VehicleLookupPage
+import pages.acquire.BusinessKeeperDetailsPage
+import uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction
+import webserviceclients.fakes.FakeAddressLookupService.addressWithUprn
+import pages.acquire.BusinessKeeperDetailsPage.{FleetNumberValid, BusinessNameValid, EmailValid, navigate, back}
+
+final class BusinessKeeperDetailsIntegrationSpec extends UiSpec with TestHarness {
+
+  "go to page" should {
+    "display the page" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to BusinessKeeperDetailsPage
+      page.title should equal(BusinessKeeperDetailsPage.title)
+    }
+
+    "display the progress of the page when progressBar is set to true" taggedAs UiTag in new ProgressBarTrue {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to BusinessKeeperDetailsPage
+      page.source.contains(progressStep(5)) should equal(true)
+    }
+
+    "not display the progress of the page when progressBar is set to false" taggedAs UiTag in new ProgressBarFalse {
+      go to BeforeYouStartPage
+      cacheSetup()
+      go to BusinessKeeperDetailsPage
+      page.source.contains(progressStep(5)) should equal(false)
+    }
+
+    "contain the hidden csrfToken field" taggedAs UiTag in new WebBrowser {
+      go to BusinessKeeperDetailsPage
+      val csrf: WebElement = webDriver.findElement(By.name(CsrfPreventionAction.TokenName))
+      csrf.getAttribute("type") should equal("hidden")
+      csrf.getAttribute("name") should equal(uk.gov.dvla.vehicles.presentation.common.filters.CsrfPreventionAction.TokenName)
+      csrf.getAttribute("value").size > 0 should equal(true)
+    }
+  }
+
+  "next button" should {
+    "go to the appropriate next page when all business keeper details are entered" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      navigate()
+      page.title should equal("") //ToDo amend title once next page is implemented
+    }
+
+    "display one validation error message when no business name is entered" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      navigate(businessName = "")
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+
+    "display one validation error message when an incorrect email is entered" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      cacheSetup()
+      navigate(email = "aaa.com")
+      ErrorPanel.numberOfErrors should equal(1)
+    }
+  }
+
+  "back" should {
+    "display previous page when back button is clicked" taggedAs UiTag in new WebBrowser {
+      go to BeforeYouStartPage
+      CookieFactoryForUISpecs.
+        setupTradeDetails().
+        dealerDetails(addressWithUprn).
+        vehicleDetails()
+
+      go to BusinessKeeperDetailsPage
+      click on back
+      page.title should equal(VehicleLookupPage.title)
+    }
+  }
+
+  private def cacheSetup()(implicit webDriver: WebDriver) =
+    CookieFactoryForUISpecs.
+      setupTradeDetails()
+      .dealerDetails()
+      .vehicleDetails()
+}
