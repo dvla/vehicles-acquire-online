4ae4e1532035e9cbd5d85462afbe1b703db50c36
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-09-17 14:29:57 +0100

    Add an ability to change the application context in the config

--- test/pages/acquire/BusinessKeeperDetailsPage.scala
+++ test/pages/acquire/BusinessKeeperDetailsPage.scala
@@ -6,7 +6,7 @@ import models.BusinessKeeperDetailsFormModel.Form.{FleetNumberId, BusinessNameId
 import views.acquire.BusinessKeeperDetails.{BackId, NextId}
 
 object BusinessKeeperDetailsPage extends Page with WebBrowserDSL {
-  final val address = s"$basePath/business-keeper-details"
+  final val address = s"$applicationContext/business-keeper-details"
   override val url: String = WebDriverFactory.testUrl + address.substring(1)
   final override val title: String = "Enter business keeper details"
 
