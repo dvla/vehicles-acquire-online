acca75676b6978e8f681c2afc359cf79292cc642
Matthew Snaith <fault_line@hotmail.co.uk>
2014-09-23 14:00:50 +0100

    BusinessKeeperDetailsCompleteUnitSpec added with implementation of date of sale for BusinessKeeperDetailsComplete

--- test/pages/acquire/BusinessKeeperDetailsCompletePage.scala
+++ test/pages/acquire/BusinessKeeperDetailsCompletePage.scala
@@ -3,7 +3,7 @@ package pages.acquire
 import helpers.webbrowser._
 import org.openqa.selenium.WebDriver
 import views.acquire.BusinessKeeperDetailsComplete.{BackId, SubmitId}
-import models.BusinessKeeperDetailsCompleteFormModel.Form.{MileageId, ConsentId}
+import models.BusinessKeeperDetailsCompleteFormModel.Form.{MileageId, ConsentId, DateOfSaleId}
 
 object BusinessKeeperDetailsCompletePage extends Page with WebBrowserDSL {
   final val address = s"$applicationContext/business-complete-and-confirm"
@@ -11,6 +11,10 @@ object BusinessKeeperDetailsCompletePage extends Page with WebBrowserDSL {
   final override val title: String = "Complete and confirm"
 
   final val MileageValid = "1000"
+  final val DayDateOfSaleValid = "19"
+  final val MonthDateOfSaleValid = "10"
+  final val YearDateOfSaleValid = "2012"
+  final val ConsentTrue = "consent"
 
   def back(implicit driver: WebDriver): Element = find(id(BackId)).get
 
@@ -18,10 +22,26 @@ object BusinessKeeperDetailsCompletePage extends Page with WebBrowserDSL {
 
   def mileageTextBox(implicit driver: WebDriver): TelField = telField(id(MileageId))
 
-  def navigate(mileage: String = MileageValid, consent: String = ConsentId)(implicit driver: WebDriver) = {
+  def dayDateOfSaleTextBox(implicit driver: WebDriver): TelField = telField(id(s"$DateOfSaleId" + "_day"))
+
+  def monthDateOfSaleTextBox(implicit driver: WebDriver): TelField = telField(id(s"$DateOfSaleId" + "_month"))
+
+  def yearDateOfSaleTextBox(implicit driver: WebDriver): TelField = telField(id(s"$DateOfSaleId" + "_year"))
+
+  def consent(implicit driver: WebDriver): Checkbox = checkbox(id(ConsentId))
+
+  def navigate(mileage: String = MileageValid,
+               dayDateOfSale: String = DayDateOfSaleValid,
+               monthDateOfSale: String = MonthDateOfSaleValid,
+               yearDateOfSale: String = YearDateOfSaleValid,
+               consent: String = ConsentId)(implicit driver: WebDriver) = {
+
     go to BusinessKeeperDetailsCompletePage
 
     mileageTextBox enter mileage
+    dayDateOfSaleTextBox enter dayDateOfSale
+    monthDateOfSaleTextBox enter monthDateOfSale
+    yearDateOfSaleTextBox enter yearDateOfSale
     click on consent
 
     click on next
