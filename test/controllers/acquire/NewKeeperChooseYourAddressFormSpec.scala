90abf7fa3ca837188fa477240da0acf7b8b8342b
Matthew Snaith <fault_line@hotmail.co.uk>
2014-10-06 10:30:50 +0100

    form tests added for NewKeeperChooseYourAddress

---
+++ test/controllers/acquire/NewKeeperChooseYourAddressFormSpec.scala
@@ -0,0 +1,48 @@
+package controllers.acquire
+
+import controllers.NewKeeperChooseYourAddress
+import helpers.UnitSpec
+import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import uk.gov.dvla.vehicles.presentation.common.webserviceclients.addresslookup.ordnanceservey.AddressLookupServiceImpl
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl.responseValidForPostcodeToAddress
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl.responseValidForPostcodeToAddressNotFound
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl.responseValidForUprnToAddress
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl.UprnValid
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl.responseValidForUprnToAddressNotFound
+import models.NewKeeperChooseYourAddressFormModel.Form.AddressSelectId
+import webserviceclients.fakes.FakeAddressLookupWebServiceImpl
+import utils.helpers.Config
+
+class NewKeeperChooseYourAddressFormSpec extends UnitSpec {
+  "form" should {
+    "accept when all fields contain valid responses" in {
+      formWithValidDefaults().get.uprnSelected should equal(UprnValid.toString)
+    }
+  }
+
+  "addressSelect" should {
+    "reject if empty" in {
+      val errors = formWithValidDefaults(addressSelected = "").errors
+      errors.length should equal(1)
+      errors(0).key should equal(AddressSelectId)
+      errors(0).message should equal("error.required")
+    }
+  }
+
+  private def businessChooseYourAddressWithFakeWebService(uprnFound: Boolean = true) = {
+    val responsePostcode = if (uprnFound) responseValidForPostcodeToAddress
+    else responseValidForPostcodeToAddressNotFound
+    val responseUprn = if (uprnFound) responseValidForUprnToAddress else responseValidForUprnToAddressNotFound
+    val fakeWebService = new FakeAddressLookupWebServiceImpl(responsePostcode, responseUprn)
+    val addressLookupService = new AddressLookupServiceImpl(fakeWebService)
+    implicit val clientSideSessionFactory = injector.getInstance(classOf[ClientSideSessionFactory])
+    implicit val config: Config = mock[Config]
+    new NewKeeperChooseYourAddress(addressLookupService)
+  }
+
+  private def formWithValidDefaults(addressSelected: String = UprnValid.toString) = {
+    businessChooseYourAddressWithFakeWebService().form.bind(
+      Map(AddressSelectId -> addressSelected)
+    )
+  }
+}
