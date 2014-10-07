ee7e1df681669ca767ab19a008628363eb008db4
Ian Stainer <ian.stainer@valtech.co.uk>
2014-10-07 15:02:18 +0100

    Refactored NewKeeperChooseYourAddress

--- app/controllers/NewKeeperChooseYourAddress.scala
+++ app/controllers/NewKeeperChooseYourAddress.scala
@@ -4,12 +4,13 @@ import javax.inject.Inject
 import play.api.Logger
 import play.api.data.{Form, FormError}
 import play.api.i18n.Lang
-import play.api.mvc.{Action, Controller, Request}
+import play.api.mvc.{AnyContent, Action, Controller, Request}
 import models.BusinessChooseYourAddressFormModel.Form.AddressSelectId
 import models.BusinessKeeperDetailsFormModel
 import models.NewKeeperChooseYourAddressFormModel
 import models.NewKeeperDetailsViewModel
 import models.PrivateKeeperDetailsFormModel
+import play.api.mvc.Result
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import uk.gov.dvla.vehicles.presentation.common
@@ -21,108 +22,103 @@ import utils.helpers.Config
 import views.html.acquire.new_keeper_choose_your_address
 
 class NewKeeperChooseYourAddress @Inject()(addressLookupService: AddressLookupService)
-                                         (implicit clientSideSessionFactory: ClientSideSessionFactory,
-                                          config: Config) extends Controller {
+                                          (implicit clientSideSessionFactory: ClientSideSessionFactory,
+                                           config: Config) extends Controller {
 
   private[controllers] val form = Form(NewKeeperChooseYourAddressFormModel.Form.Mapping)
 
-  def present = Action.async { implicit request => request.cookies.getModel[PrivateKeeperDetailsFormModel] match {
-      case Some(privateKeeperDetails) =>
-        val session = clientSideSessionFactory.getSession(request.cookies)
-        fetchPrivateKeeperAddresses(privateKeeperDetails)(session, request2lang).map { addresses =>
-          Ok(views.html.acquire.new_keeper_choose_your_address(
-            form.fill(),
-            privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName,
-            privateKeeperDetails.postcode,
-            privateKeeperDetails.email.getOrElse("Not entered"),
-            addresses))
-        }
-      case None => request.cookies.getModel[BusinessKeeperDetailsFormModel] match {
-          case Some(businessKeeperDetails) =>
-            val session = clientSideSessionFactory.getSession(request.cookies)
-            fetchBusinessKeeperAddresses(businessKeeperDetails)(session, request2lang).map { addresses =>
-              Ok(views.html.acquire.new_keeper_choose_your_address(
-                form.fill(),
-                businessKeeperDetails.businessName,
-                businessKeeperDetails.postcode,
-                businessKeeperDetails.email.getOrElse("Not entered"),
-                addresses))
-            }
-          case None => Future {
-            Logger.error("Failed to find keeper details in cache. Now redirecting to vehicle lookup.")
-            Redirect(routes.VehicleLookup.present())
-          }
-        }
+  private final val KeeperDetailsNotInCacheMessage = "Failed to find keeper details in cache. Now redirecting to vehicle lookup."
+  private def keeperDetailsNotInCacheCall = routes.VehicleLookup.present()
+
+  private def switch[R](request: Request[AnyContent],
+                     priv: PrivateKeeperDetailsFormModel => R,
+                     business: BusinessKeeperDetailsFormModel => R,
+                     neither: => R): R = {
+    val privateKeeperDetailsOpt = request.cookies.getModel[PrivateKeeperDetailsFormModel]
+    val businessKeeperDetailsOpt = request.cookies.getModel[BusinessKeeperDetailsFormModel]
+    (privateKeeperDetailsOpt, businessKeeperDetailsOpt) match {
+      case (Some(privateKeeperDetails), Some(businessKeeperDetails)) => neither
+      case (Some(privateKeeperDetails), _) => priv(privateKeeperDetails)
+      case (_, Some(businessKeeperDetails)) => business(businessKeeperDetails)
+      case _ => neither
     }
   }
 
+  private def neither: Result = {
+    Logger.warn(KeeperDetailsNotInCacheMessage)
+    Redirect(keeperDetailsNotInCacheCall)
+  }
+
+  def present = Action.async { implicit request => switch(request, { privateKeeperDetails =>
+    val session = clientSideSessionFactory.getSession(request.cookies)
+    fetchAddresses(privateKeeperDetails.postcode)(session, request2lang).map { addresses =>
+      openView(privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName,
+        privateKeeperDetails.postcode,
+        privateKeeperDetails.email,
+        addresses
+      )
+    }
+  }, { businessKeeperDetails =>
+    val session = clientSideSessionFactory.getSession(request.cookies)
+    fetchAddresses(businessKeeperDetails.postcode)(session, request2lang).map { addresses =>
+      openView(businessKeeperDetails.businessName,
+        businessKeeperDetails.postcode,
+        businessKeeperDetails.email,
+        addresses
+      )
+    }
+  }, Future(neither))
+  }
+
+  private def openView(name: String, postcode: String, email: Option[String], addresses: Seq[(String, String)])
+                      (implicit request: Request[_]) =
+    Ok(views.html.acquire.new_keeper_choose_your_address(
+      form.fill(), name, postcode, email.getOrElse("Not entered"), addresses))
+
   def submit = Action.async { implicit request =>
     form.bindFromRequest.fold(
-      invalidForm => request.cookies.getModel[PrivateKeeperDetailsFormModel] match {
-          case Some(privateKeeperDetails) =>
-            implicit val session = clientSideSessionFactory.getSession(request.cookies)
-            fetchPrivateKeeperAddresses(privateKeeperDetails).map { addresses =>
-              BadRequest(new_keeper_choose_your_address(formWithReplacedErrors(invalidForm),
-                privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName,
-                privateKeeperDetails.postcode,
-                privateKeeperDetails.email.getOrElse("Not entered"),
-                addresses))
-            }
-          case None => request.cookies.getModel[BusinessKeeperDetailsFormModel] match {
-              case Some(businessKeeperDetails) =>
-                implicit val session = clientSideSessionFactory.getSession(request.cookies)
-                fetchBusinessKeeperAddresses(businessKeeperDetails).map { addresses =>
-                  BadRequest(new_keeper_choose_your_address(formWithReplacedErrors(invalidForm),
-                    businessKeeperDetails.businessName,
-                    businessKeeperDetails.postcode,
-                    businessKeeperDetails.email.getOrElse("Not entered"),
-                    addresses))
-                }
-              case None => Future {
-                Logger.error("Failed to find keeper details in cache. Now redirecting to vehicle lookup.")
-                Redirect(routes.VehicleLookup.present())
-              }
-            }
-        },
+      invalidForm =>
+        switch(request, { privateKeeperDetails =>
+          implicit val session = clientSideSessionFactory.getSession(request.cookies)
+          fetchAddresses(privateKeeperDetails.postcode).map { addresses =>
+            BadRequest(new_keeper_choose_your_address(formWithReplacedErrors(invalidForm),
+              privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName,
+              privateKeeperDetails.postcode,
+              privateKeeperDetails.email.getOrElse("Not entered"),
+              addresses))
+          }
+        }, { businessKeeperDetails =>
+          implicit val session = clientSideSessionFactory.getSession(request.cookies)
+          fetchAddresses(businessKeeperDetails.postcode).map { addresses =>
+            BadRequest(new_keeper_choose_your_address(formWithReplacedErrors(invalidForm),
+              businessKeeperDetails.businessName,
+              businessKeeperDetails.postcode,
+              businessKeeperDetails.email.getOrElse("Not entered"),
+              addresses))
+          }
+        }, Future(neither)),
       validForm =>
-        request.cookies.getModel[PrivateKeeperDetailsFormModel] match {
-          case Some(privateKeeperDetails) =>
-            println("Private keeper details form model match, looking up uprn")
-            implicit val session = clientSideSessionFactory.getSession(request.cookies)
-            lookupUprn(validForm, privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName, privateKeeper = true)
-          case None => request.cookies.getModel[BusinessKeeperDetailsFormModel] match {
-              case Some(businessKeeperDetails) =>
-                implicit val session = clientSideSessionFactory.getSession(request.cookies)
-                lookupUprn(validForm, businessKeeperDetails.businessName, privateKeeper = false)
-              case None => Future {
-                Logger.error("Failed to find new keeper details, redirecting")
-                Redirect(routes.VehicleLookup.present())
-              }
-            }
-        }
+        switch(request, { privateKeeperDetails =>
+          println("Private keeper details form model match, looking up uprn")
+          implicit val session = clientSideSessionFactory.getSession(request.cookies)
+          lookupUprn(validForm, privateKeeperDetails.firstName + " " + privateKeeperDetails.lastName, privateKeeper = true)
+        }, { businessKeeperDetails =>
+          implicit val session = clientSideSessionFactory.getSession(request.cookies)
+          lookupUprn(validForm, businessKeeperDetails.businessName, privateKeeper = false)
+        }, Future(neither))
     )
   }
 
   def back = Action { implicit request =>
-    val privateKeeperDetailsOpt = request.cookies.getModel[PrivateKeeperDetailsFormModel]
-    val businessKeeperDetailsOpt = request.cookies.getModel[BusinessKeeperDetailsFormModel]
-    (privateKeeperDetailsOpt, businessKeeperDetailsOpt) match {
-      case (Some(privateKeeperDetails), _) =>
-        Redirect(routes.PrivateKeeperDetails.present())
-      case (_, Some(businessKeeperDetails)) =>
-        Redirect(routes.BusinessKeeperDetails.present())
-      case _ =>
-        // This should never happen because this case is guarded in the present method
-        Logger.warn("Failed to find a cookie for the new keeper. Now redirecting to vehicle lookup.")
-        Redirect(routes.VehicleLookup.present())
-    }
+    switch(request, { privateKeeperDetails =>
+      Redirect(routes.PrivateKeeperDetails.present())
+    }, { businessKeeperDetails =>
+      Redirect(routes.BusinessKeeperDetails.present())
+    }, neither)
   }
 
-  private def fetchPrivateKeeperAddresses(model: PrivateKeeperDetailsFormModel)(implicit session: ClientSideSession, lang: Lang) =
-    addressLookupService.fetchAddressesForPostcode(model.postcode, session.trackingId)
-
-  private def fetchBusinessKeeperAddresses(model: BusinessKeeperDetailsFormModel)(implicit session: ClientSideSession, lang: Lang) =
-    addressLookupService.fetchAddressesForPostcode(model.postcode, session.trackingId)
+  private def fetchAddresses(postcode: String)(implicit session: ClientSideSession, lang: Lang) =
+    addressLookupService.fetchAddressesForPostcode(postcode, session.trackingId)
 
   private def formWithReplacedErrors(form: Form[NewKeeperChooseYourAddressFormModel])(implicit request: Request[_]) =
     form.replaceError(AddressSelectId, "error.required",
