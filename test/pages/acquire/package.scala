dfc64782c9cd35974c1d2418145e0dbfb4f7d1e7
Oliver Wickham <mr.o.wickham@gmail.com>
2014-10-08 12:13:56 +0100

    Add method to build app urls correctly

--- test/pages/acquire/package.scala
+++ test/pages/acquire/package.scala
@@ -6,7 +6,7 @@ package object acquire {
   private final val applicationContext = getProperty("application.context", default = "/")
 
   def buildAppUrl(urlPart: String) = {
-    val appContextWithSlash = if (!applicationContext.endsWith("/")) s"/$applicationContext" else applicationContext
+    val appContextWithSlash = if (!applicationContext.endsWith("/")) s"$applicationContext/" else applicationContext
     val urlPartWithoutSlash = urlPart.dropWhile(_ == '/')
 
     appContextWithSlash + urlPartWithoutSlash
