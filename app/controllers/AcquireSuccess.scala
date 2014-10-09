be3dcefb5538cc8523af7ca20c7b014d15c95e45
Matthew Snaith <fault_line@hotmail.co.uk>
2014-10-09 14:12:21 +0100

    amendments to AcquireSuccess controller and view

--- app/controllers/AcquireSuccess.scala
+++ app/controllers/AcquireSuccess.scala
@@ -1,14 +1,28 @@
 package controllers
 
 import com.google.inject.Inject
+import models.NewKeeperDetailsViewModel
 import play.api.mvc.{Action, Controller}
-import uk.gov.dvla.vehicles.presentation.common.clientsidesession.ClientSideSessionFactory
+import play.api.Logger
+import uk.gov.dvla.vehicles.presentation.common
+import common.clientsidesession.ClientSideSessionFactory
+import common.clientsidesession.CookieImplicits.{RichCookies, RichForm, RichResult}
+import common.services.DateService
+import common.views.helpers.FormExtensions.formBinding
 import utils.helpers.Config
+
+import uk.gov.dvla.vehicles.presentation.common.model.{VehicleDetailsModel, TraderDetailsModel}
 
 final class AcquireSuccess @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                        config: Config) extends Controller {
 
   def present = Action { implicit request =>
-    Ok(views.html.acquire.acquire_success())
+    (request.cookies.getModel[NewKeeperDetailsViewModel],
+     request.cookies.getModel[TraderDetailsModel],
+     request.cookies.getModel[VehicleDetailsModel]) match {
+      case (Some(newKeeperDetails), Some(traderDetails), Some(vehicleDetails)) =>
+        Ok(views.html.acquire.acquire_success())
+      case _ => Ok(views.html.acquire.acquire_success())
+    }
   }
 }
---
+++ app/models/AcquireSuccessViewModel.scala
@@ -0,0 +1,9 @@
+package models
+
+import uk.gov.dvla.vehicles.presentation.common.model.{TraderDetailsModel, VehicleDetailsModel}
+
+final case class AcquireSuccessViewModel(vehicleDetails: VehicleDetailsModel,
+                                         traderDetails: TraderDetailsModel,
+                                         newKeeperDetails: NewKeeperDetailsViewModel) {
+
+}
