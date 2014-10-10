3708b689bac48fc32d8be5199ed6401b929bf782
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-10-10 18:10:47 +0100

    Add the buy another vehicle button

--- app/controllers/AcquireSuccess.scala
+++ app/controllers/AcquireSuccess.scala
@@ -2,19 +2,23 @@ package controllers
 
 import com.google.inject.Inject
 import models.{AcquireSuccessViewModel, CompleteAndConfirmFormModel, NewKeeperDetailsViewModel}
+import NewKeeperDetailsViewModel.NewKeeperDetailsCacheKey
+import uk.gov.dvla.vehicles.presentation.common.model.{VehicleDetailsModel, TraderDetailsModel}
+import VehicleDetailsModel.VehicleLookupDetailsCacheKey
+import CompleteAndConfirmFormModel.CompleteAndConfirmCacheKey
 import play.api.Logger
 import play.api.mvc.{Action, Controller}
 import uk.gov.dvla.vehicles.presentation.common
 import common.clientsidesession.ClientSideSessionFactory
-import common.clientsidesession.CookieImplicits.RichCookies
+import common.clientsidesession.CookieImplicits.{RichCookies, RichResult}
 import common.model.{TraderDetailsModel, VehicleDetailsModel}
 import utils.helpers.Config
 
 final class AcquireSuccess @Inject()()(implicit clientSideSessionFactory: ClientSideSessionFactory,
                                        config: Config) extends Controller {
 
-  def present = Action { implicit request => {
-    (for {
+  def present = Action { implicit request =>
+    val result = for {
       newKeeperDetails <- request.cookies.getModel[NewKeeperDetailsViewModel]
       traderDetails <- request.cookies.getModel[TraderDetailsModel]
       vehicleDetails <- request.cookies.getModel[VehicleDetailsModel]
@@ -26,9 +30,23 @@ final class AcquireSuccess @Inject()()(implicit clientSideSessionFactory: Client
         newKeeperDetails = newKeeperDetails,
         completeAndConfirmDetails = completeAndConfirmDetails
       )))
-    ) getOrElse {
+    result getOrElse {
       Logger.warn("missing cookies in cache. Acquire successful, however cannot display success page")
       Redirect(routes.BeforeYouStart.present())
     }
-  }}
+  }
+
+  def buyAnother = Action { implicit request =>
+    val result = for {
+      newKeeperDetails <- request.cookies.getModel[NewKeeperDetailsViewModel]
+      traderDetails <- request.cookies.getModel[TraderDetailsModel]
+      vehicleDetails <- request.cookies.getModel[VehicleDetailsModel]
+      completeAndConfirmDetails <- request.cookies.getModel[CompleteAndConfirmFormModel]
+    } yield Redirect(routes.VehicleLookup.present())
+        .discardingCookies(Set(NewKeeperDetailsCacheKey, VehicleLookupDetailsCacheKey, CompleteAndConfirmCacheKey))
+    result getOrElse {
+      Logger.warn("missing cookies in cache.")
+      Redirect(routes.BeforeYouStart.present())
+    }
+  }
 }
