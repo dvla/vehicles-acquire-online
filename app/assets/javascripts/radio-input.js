e3e0e2b3e44ee58acfe277dce9c992106c6aeacb
Tony Barnes <tony@tonybarnes.me>
2014-10-21 10:46:34 +0100

    refactor radio input selection script to handle checkboxes

--- app/assets/javascripts/radio-input.js
+++ app/assets/javascripts/radio-input.js
@@ -1,18 +1,20 @@
-//radio button selection, active class
+//radio and checkbox selection, active class toggle
 require(["jquery"],function($) {
 
-  var radiosWrap = $('div.radio');
-  var radioLabel = radiosWrap.find('.form-radio.selectable');
-  var radio = radioLabel.children('input:radio');
-
-  radio.each(function() {
-    $(this).on('change', function() {
-      if($('input:radio:checked').length > 0){
-        $('input:radio').parent('label').removeClass('selected');
-        $(this).parent('label').addClass("selected");
-      } else if (!$('input:radio:checked').length > 0){
-          $(this).parent('label').removeClass("selected");
+  var label = $('label.form-radio.selectable, label.form-checkbox.selectable');
+  
+  label.each(function() {
+    var input = $(this).children('input:radio, input:checkbox');
+    input.on('change', function() {
+      if(input.is(':checked')) {
+        $('label').removeClass('selected');
+        $(this).parent('label').addClass('selected');
+        input.parent(label).addClass('selected');
+      } 
+      if(!input.is(':checked')) {
+        input.parent(label).removeClass('selected');
       }
     });
   });
+
 });
