2ee181f07e667b9e1ad0e3173d47a26f6754da0f
Matthew Snaith <fault_line@hotmail.co.uk>
2014-10-23 13:29:22 +0100

    amended ordering of replacing error in PrivateKeeperDetails to ensure error messages are replaced and displayed in the correct order

--- app/controllers/PrivateKeeperDetails.scala
+++ app/controllers/PrivateKeeperDetails.scala
@@ -9,6 +9,7 @@ import models.PrivateKeeperDetailsFormModel.Form.EmailId
 import models.PrivateKeeperDetailsFormModel.Form.FirstNameId
 import models.PrivateKeeperDetailsFormModel.Form.LastNameId
 import models.PrivateKeeperDetailsFormModel.Form.PostcodeId
+import models.PrivateKeeperDetailsFormModel.Form.DateOfBirthId
 import play.api.data.{FormError, Form}
 import uk.gov.dvla.vehicles.presentation.common
 import common.clientsidesession.ClientSideSessionFactory
@@ -46,20 +47,17 @@ final class PrivateKeeperDetails @Inject()()(implicit clientSideSessionFactory: 
 
   private def formWithReplacedErrors(form: Form[PrivateKeeperDetailsFormModel]) = {
     form.replaceError(
-//      FirstNameId,
-//      FormError(key = FirstNameId, message = "error.validFirstName", args = Seq.empty)
-//    ).
-//      replaceError(
-        LastNameId,
-        FormError(key = LastNameId,message = "error.validLastName", args = Seq.empty)
-      ).
-      replaceError(
-        DriverNumberId,
-        FormError(key = DriverNumberId, message = "error.validDriverNumber", args = Seq.empty)
-      ).
-      replaceError(
-        PostcodeId,
-        FormError(key = PostcodeId, message = "error.restricted.validPostcode", args = Seq.empty)
+      FirstNameId, FormError(key = FirstNameId, message = "error.titlePlusFirstName.tooLong", args = Seq.empty)
+    ).replaceError(
+        LastNameId, FormError(key = LastNameId,message = "error.validLastName", args = Seq.empty)
+      ).replaceError(
+        DateOfBirthId, FormError(key = DateOfBirthId, message = "error.date.invalid", args = Seq.empty)
+      ).replaceError(
+        DriverNumberId, FormError(key = DriverNumberId, message = "error.validDriverNumber", args = Seq.empty)
+      ).replaceError(
+        EmailId, FormError(key = EmailId, message = "error.email", args = Seq.empty)
+      ).replaceError(
+        PostcodeId, FormError(key = PostcodeId, message = "error.restricted.validPostcode", args = Seq.empty)
       ).distinctErrors
   }
 
