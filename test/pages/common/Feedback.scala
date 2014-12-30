46dcac3be10491d092943b748fb251e6158608f1
Gerasimos.Arvanitis <gerasimos.arvanitis@valtech.co.uk>
2014-12-30 14:04:38 +0100

    US2011: Fix failing tests

--- test/pages/common/Feedback.scala
+++ test/pages/common/Feedback.scala
@@ -1,6 +1,5 @@
 package pages.common
 
 object Feedback {
-  final val AcquireEmailFeedbackLink = "<a id=\"feedback\" target=\"_blank\" href=\"mailto:vm.feedback@digital.dvla.gov.uk?" +
-    "Subject=Buy%20from%20the%20trade%20feedback\">"
+  final val AcquireEmailFeedbackLink = "<a id=\"feedback\" target=\"_blank\" href=\"/feedback\">"
 }
