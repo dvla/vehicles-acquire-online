2f56ccc060c80cad00ecf5a90296463cf23cdb71
Edmund <esr360@live.com>
2015-01-08 11:21:44 +0000

    US2011 - changing feedback form to open by window.open

--- test/pages/common/Feedback.scala
+++ test/pages/common/Feedback.scala
@@ -1,5 +1,5 @@
 package pages.common
 
 object Feedback {
-  final val AcquireEmailFeedbackLink = "<a id=\"feedback\" target=\"_blank\" href=\"/feedback\">"
+  final val AcquireEmailFeedbackLink = s"""<a id="${views.common.Feedback.FeedbackId}" href="" onclick="window.open('${controllers.routes.FeedbackController.present()}', '_blank')">"""
 }
