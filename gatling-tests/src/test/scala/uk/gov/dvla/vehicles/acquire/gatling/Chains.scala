1a1a3c44faa5036aec7428050b5644b94418d7a8
Subbu.Akepati <subbu.akepati@valtech.co.uk>
2015-01-28 12:39:20 +0000

    US2228 enhance gatling tests for optional amd mondetory fields

--- gatling-tests/src/test/scala/uk/gov/dvla/vehicles/acquire/gatling/Chains.scala
+++ gatling-tests/src/test/scala/uk/gov/dvla/vehicles/acquire/gatling/Chains.scala
@@ -3,119 +3,319 @@ package uk.gov.dvla.vehicles.acquire.gatling
 import io.gatling.core.Predef._
 import io.gatling.core.feeder.RecordSeqFeederBuilder
 import io.gatling.http.Predef._
-import uk.gov.dvla.vehicles.acquire.gatling.Headers
-import uk.gov.dvla.vehicles.acquire.gatling.Headers.headers_accept_html
+import Headers.{headers_accept_html, headers_accept_png, headers_x_www_form_urlencoded}
 
 class Chains(data: RecordSeqFeederBuilder[String]) {
 
-  private val beforeYouStartPageTitle = "Buy a vehicle from the motor trade"
-  private val setupTradeDetailsPageTitle = "Provide trader details"
+  private final val BeforeYouStartPageTitle = "Buy a vehicle from the motor trade"
+  private final val SetupTradeDetailsPageTitle = "Provide trader details"
+  private final val BusinessChooseYourAddressPageTitle = "Select trader address"
+  private final val TraderPlayBackHeading = "Vehicle being sold from"
+  private final val VehicleLookupPageTitle = "Enter vehicle details"
+  private final val BusinessKeeperDetailsPageTitle = "Enter business keeper details"
+  private final val PrivateKeeperDetailsPageTitle = "Enter new keeper details"
+  private final val NewKeeperChooseYourAddressPageTitle = "Select new keeper address"
+  private final val VehicleTaxOrSorn = "Vehicle tax or SORN"
+  private final val CompleteAndConfirmPageTitle = "Complete and confirm"
+  private final val VehicleDetailsPlaybackHeading = "Vehicle details"
+  private final val KeeperDetailsPlaybackHeading = "New keeper details"
+  private final val SummaryPageTitle = "Summary"
+  private final val TransactionDetailsPlaybackHeading = "Transaction details"
+  private final val VehicleLookupFailurePageTitle = "Look-up was unsuccessful"
 
-  private val assets200 =
-    exec(
-      http("screen.min.css")
-        .get( s"""/assets/screen.min.css""")
+  def verifyAssetsAreAccessible =
+    exec(http("screen.min.css")
+      .get(s"/assets/screen.min.css")
     )
-      /*.exec(
-      http(s"GET /assets/fonts.min.css")
-        .get(s"""/assets/fonts.min.css""")
-      )*/
-      .exec(
-        http("print.min.css")
-          .get( s"""/assets/print.min.css""")
+      .exec(http("print.min.css")
+      .get(s"/assets/print.min.css")
       )
       .exec(
         http("govuk-crest.png")
-          .get( s"""/assets/lib/vehicles-presentation-common/images/govuk-crest.png""")
-          .headers(Map( """Accept""" -> """image/png,image/*;q=0.8,*/*;q=0.5"""))
-      )
-      .exec(
-        http("require.js")
-          .get( s"""/assets/javascripts/require.js""")
-          .headers(Map( """Accept""" -> """*/*"""))
-      )
-      .exec(
-        http("main.js")
-          .get( s"""/assets/javascripts/main.js""")
-          .headers(Map( """Accept""" -> """*/*"""))
-      )
-
-  private val assets304 =
-    exec(
-      http("screen.min.css")
-        .get( s"""/assets/screen.min.css""")
-        .headers(Map(
-        """If-Modified-Since""" -> """Thu, 05 Jun 2014 21:08:06 GMT""",
-        """If-None-Match""" -> """59f34576dba4629e6e960e1d514fe573775e9999"""))
-      //.check(status.is(304))
-    )
-      /*.exec(
-      http("fonts.min.css")
-        .get(s"""/assets/fonts.min.css""")
-        .headers(Map(
-          """If-Modified-Since""" -> """Fri, 30 May 2014 13:08:32 GMT""",
-          """If-None-Match""" -> """0702d8d00d43562d6fa1a4e87ac82609dc70ffc9"""))
-        .check(status.is(304))
-      )*/
-      .exec(
-        http("print.min.css")
-          .get( s"""/assets/print.min.css""")
-          .headers(Map(
-          """If-Modified-Since""" -> """Thu, 05 Jun 2014 21:08:08 GMT""",
-          """If-None-Match""" -> """b2b112249c52769ac41acd83e388f550e4c39c6f"""))
-        //.check(status.is(304))
-      )
-      .exec(
-        http("require.js")
-          .get( s"""/assets/javascripts/require.js""")
-          .headers(Map(
-          """Accept""" -> """*/*""",
-          """If-Modified-Since""" -> """Tue, 06 Aug 2013 09:49:32 GMT""",
-          """If-None-Match""" -> """858bab5a8e8f73a1d706221ed772a4f740e168d5"""))
-        //.check(status.is(304))
+          .get(s"/assets/lib/vehicles-presentation-common/images/govuk-crest.png")
+          .headers(headers_accept_png)
       )
       .exec(
         http("govuk-crest.png")
-          .get( s"""/assets/lib/vehicles-presentation-common/images/govuk-crest.png""")
-          .headers(Map(
-          """Accept""" -> """image/png,image/*;q=0.8,*/*;q=0.5""",
-          """If-Modified-Since""" -> """Thu, 22 May 2014 14:25:18 GMT""",
-          """If-None-Match""" -> """0464ba08d53d88645ca77f9907c082c8c10d563b"""))
-        //.check(status.is(304))
+          .get(s"/assets/lib/vehicles-presentation-common/images/govuk-crest-2x.png")
+          .headers(headers_accept_png)
       )
       .exec(
-        http("main.js")
-          .get( s"""/assets/javascripts/main.js""")
-          .headers(Map(
-          """Accept""" -> """*/*""",
-          """If-Modified-Since""" -> """Thu, 05 Jun 2014 21:10:42 GMT""",
-          """If-None-Match""" -> """5f859f72e7cc426915cf32f2643ee5fc494b04a8"""))
-        //.check(status.is(304))
+        http("govuk-crest.png")
+          .get(s"/assets/lib/vehicles-presentation-common/images/gov.uk_logotype_crown.png")
+          .headers(headers_accept_png)
+      )
+      .exec(
+        http("govuk-crest.png")
+          .get(s"/assets/lib/vehicles-presentation-common/images/open-government-licence_2x.png")
+          .headers(headers_accept_png)
+      )
+      .exec(http("require.js")
+      .get(s"/assets/javascripts/require.js")
+      .headers(Map( """Accept""" -> """*/*"""))
+      )
+      .exec(http("custom.js")
+      .get(s"/assets/javascripts/main.js")
+      .headers(Map( """Accept""" -> """*/*"""))
       )
 
-  def beforeYouStart =
+  def beforeYouStart = {
+    val url = "/before-you-start"
+    val chainTitle = s"GET $url"
     exitBlockOnFail(
       exec(
-        http("beforeYouStart")
-          .get(s"/before-you-start")
+        http(chainTitle)
+          .get(url)
           .headers(headers_accept_html)
           // Assertions
           .check(status.is(200))
-          .check(regex(beforeYouStartPageTitle).exists) // Page title
+          .check(regex(BeforeYouStartPageTitle).exists)
       )
-        .exec(assets200)
     )
+  }
 
-  def beforeYouStartToProvideTradeDetails =
+  def traderDetailsPage = {
+    val url = "/setup-trade-details"
+    val chainTitle = s"GET $url"
     exitBlockOnFail(
       exec(
-        http("beforeYouStartToProvideTradeDetails")
-          .get(s"/setup-trade-details") // Gatlin doesn't click on links, you need to go to the next page.
+        http(chainTitle)
+          .get(url)
           .headers(headers_accept_html)
-          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
           // Assertions
-          .check(regex(setupTradeDetailsPageTitle).exists) // Page title
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(SetupTradeDetailsPageTitle).exists)
       )
-        .exec(assets304) // check the assets haven't changed
     )
+  }
+
+  def traderDetailsSubmit = {
+    val url = "/setup-trade-details"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      feed(data)
+        .exec(
+          http(chainTitle)
+            .post(url)
+            .headers(headers_accept_html)
+            .formParam("traderName", "${traderName}")
+            .formParam("traderPostcode", "${traderPostcode}")
+            .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+            .formParam("action", "")
+            // Assertions
+            .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+            .check(regex(BusinessChooseYourAddressPageTitle).exists)
+        )
+    )
+  }
+
+  def businessChooseYourAddress = {
+
+    val url = "/business-choose-your-address"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("disposal_businessChooseYourAddress_addressSelect", "0") // UPRN disabled for Northern Ireland
+          .check(regex(VehicleLookupPageTitle).exists)
+          .check(regex(TraderPlayBackHeading).exists)
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+
+      )
+    )
+  }
+
+  def vehicleLookup = {
+    val url = "/vehicle-lookup"
+    val chainTitle = s"GET $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .get(url)
+          .headers(headers_accept_html)
+          .check(status.is(200))
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(TraderPlayBackHeading).exists)
+          .check(regex(VehicleLookupPageTitle).exists)
+          .check(regex("${expected_traderName}").exists)
+      )
+    )
+  }
+
+  def vehicleLookupSubmitNewBusinessKeeper = vehicleLookupSubmit(BusinessKeeperDetailsPageTitle)
+  def vehicleLookupSubmitNewPrivateKeeper = vehicleLookupSubmit(PrivateKeeperDetailsPageTitle)
+
+  private def vehicleLookupSubmit(expectedPageTitle: String) = {
+    val url = "/vehicle-lookup"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      feed(data)
+        .exec(
+          http(chainTitle)
+            .post(url)
+            .headers(headers_x_www_form_urlencoded)
+            .formParam("vehicleRegistrationNumber", "${vehicleRegistrationNumber}")
+            .formParam("documentReferenceNumber", "${documentReferenceNumber}")
+            .formParam("vehicleSoldTo", "${vehicleSoldTo}")
+            .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+            .formParam("action", "")
+            // Assertions
+            .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+            .check(regex(expectedPageTitle).exists)
+            .check(regex(VehicleDetailsPlaybackHeading).exists)
+            .check(regex("${expected_registrationNumberFormatted}").exists)
+            .check(regex("${expected_make}").exists)
+        )
+    )
+  }
+
+  def businessKeeperDetailsSubmit = {
+    val url = "/business-keeper-details"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("fleetNumber", "${fleetNumber}")
+          .formParam("businessName", "${businessName}")
+          .formParam("businessKeeper_email", "${businessEmail}")
+          .formParam("businesskeeper_postcode", "${businessPostcode}")
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(NewKeeperChooseYourAddressPageTitle).exists)
+          .check(regex(VehicleDetailsPlaybackHeading).exists)
+          .check(regex("${expected_registrationNumberFormatted}").exists)
+          .check(regex("${expected_make}").exists)
+          .check(regex("${expected_model}").exists)
+      )
+    )
+  }
+
+  def newKeeperChooseYourAddressSubmit = {
+    val url = "/new-keeper-choose-your-address"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("acquire_newKeeperChooseYourAddress_addressSelect", "0") // UPRN disabled for Northern Ireland
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(VehicleTaxOrSorn).exists)
+          .check(regex(KeeperDetailsPlaybackHeading).exists)
+          .check(regex("${expected_buyerName}").exists)
+          .check(regex("${expected_buyerAddressLine1}").exists)
+          .check(regex("${expected_buyerAddressLine2}").exists)
+          .check(regex("${expected_buyerAddressPostcode}").exists)
+          .check(regex(VehicleDetailsPlaybackHeading).exists)
+          .check(regex("${expected_registrationNumberFormatted}").exists)
+          .check(regex("${expected_make}").exists)
+          .check(regex("${expected_model}").exists)
+      )
+    )
+  }
+
+  def privateKeeperDetailsSubmit = {
+    val url = "/private-keeper-details"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("privatekeeper_title.titleOption", "${privateKeeperTitle}")
+          .formParam("privatekeeper_title.titleText", "${privateKeeperTitleText}")
+          .formParam("privatekeeper_firstname", "${privateKeeperFirstName}")
+          .formParam("privatekeeper_lastname", "${privateKeeperLastName}")
+          .formParam("privatekeeper_dateofbirth.day", "${dateOfBirthDay}")
+          .formParam("privatekeeper_dateofbirth.month", "${dateOfBirthMonth}")
+          .formParam("privatekeeper_dateofbirth.year", "${dateOfBirthYear}")
+          .formParam("privatekeeper_drivernumber", "${driverNumber}")
+          .formParam("privatekeeper_email", "${email}")
+          .formParam("privatekeeper_postcode", "${privateKeeperPostcode}")
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(NewKeeperChooseYourAddressPageTitle).exists)
+          .check(regex(VehicleDetailsPlaybackHeading).exists)
+          .check(regex("${expected_registrationNumberFormatted}").exists)
+          .check(regex("${expected_make}").exists)
+          .check(regex("${expected_model}").exists)
+      )
+    )
+  }
+
+  def taxOrSornVehicle = {
+    val url = "/vehicle-tax-or-sorn"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(CompleteAndConfirmPageTitle).exists)
+      )
+    )
+  }
+
+  def completeAndConfirmSubmit = {
+    val url = "/complete-and-confirm"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      exec(
+        http(chainTitle)
+          .post(url)
+          .headers(headers_x_www_form_urlencoded)
+          .formParam("mileage", "${mileage}")
+          .formParam("dateofsale.day", "${dateDay}")
+          .formParam("dateofsale.month", "${dateMonth}")
+          .formParam("dateofsale.year", "${dateYear}")
+          .formParam("consent", "${consent}")
+          .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+          .formParam("action", "")
+          // Assertions
+          .check(regex( """<input type="hidden" name="csrf_prevention_token" value="(.*)"/>""").saveAs("csrf_prevention_token"))
+          .check(regex(SummaryPageTitle).exists)
+          .check(regex(TransactionDetailsPlaybackHeading).exists)
+          .check(regex("${expected_transactionId}").exists)
+          .check(regex("${expected_transactionDate}").exists)
+      )
+    )
+  }
+
+  def vehicleLookupUnsuccessfulSubmit = {
+    val url = "/vehicle-lookup"
+    val chainTitle = s"POST $url"
+    exitBlockOnFail(
+      feed(data)
+        .exec(
+          http(chainTitle)
+            .post(url)
+            .headers(headers_x_www_form_urlencoded)
+            .formParam("vehicleRegistrationNumber", "${vehicleRegistrationNumber}")
+            .formParam("documentReferenceNumber", "${documentReferenceNumber}")
+            .formParam("vehicleSoldTo", "${vehicleSoldTo}")
+            .formParam("csrf_prevention_token", "${csrf_prevention_token}")
+            .formParam("action", "")
+            // Assertions
+            .check(regex(VehicleLookupFailurePageTitle).exists)
+        )
+    )
+  }
 }
--- gatling-tests/src/test/scala/uk/gov/dvla/vehicles/acquire/gatling/Scenarios.scala
+++ gatling-tests/src/test/scala/uk/gov/dvla/vehicles/acquire/gatling/Scenarios.scala
@@ -1,24 +1,112 @@
 package uk.gov.dvla.vehicles.acquire.gatling
 
 import io.gatling.core.Predef._
+import io.gatling.core.feeder._
 
 object Scenarios {
 
-  def beforeYouStart = {
-    val data = csv("data/BeforeYouStart.csv").circular
-    val chain = new Chains(data)
-    endToEnd(
-      scenarioName = "A dummy gatling trip through the pages. Will change as more pages are developed",
-      chain
-    )
+  def verifyAssetsAreAccessible = {
+    val noData = RecordSeqFeederBuilder[String](records = IndexedSeq.empty[Record[String]])
+    val chain = new Chains(noData)
+    scenario("Verify assets are accessible")
+      .exec(
+        chain.verifyAssetsAreAccessible
+      )
   }
 
-  private def endToEnd(scenarioName: String, chain: Chains) =
-    scenario(scenarioName)
+  def buyFromTraderForBusiness = {
+    val data = csv("data/happy/BuyAVehicleFromTheTraderToBusiness.csv").circular
+    val chain = new Chains(data)
+    scenario("Buy a vehicle from the motor trader as a new business keeper from start to finish")
       .exitBlockOnFail(
         exec(
           chain.beforeYouStart,
-          chain.beforeYouStartToProvideTradeDetails
+          chain.traderDetailsPage,
+          chain.traderDetailsSubmit,
+          chain.businessChooseYourAddress,
+          chain.vehicleLookup,
+          chain.vehicleLookupSubmitNewBusinessKeeper,
+          chain.businessKeeperDetailsSubmit,
+          chain.newKeeperChooseYourAddressSubmit,
+          chain.taxOrSornVehicle,
+          chain.completeAndConfirmSubmit
         )
       )
+  }
+
+  def buyFromTraderForBusinessAllOptionalDataFilledIn = {
+    val data = csv("data/happy/BuyAVehicleFromTheTraderToBusinessAllOptionalDataFilledIn.csv").circular
+    val chain = new Chains(data)
+    scenario("Buy a vehicle from the motor trader as a new business keeper from start to finish with all optional data filled in")
+      .exitBlockOnFail(
+        exec(
+          chain.beforeYouStart,
+          chain.traderDetailsPage,
+          chain.traderDetailsSubmit,
+          chain.businessChooseYourAddress,
+          chain.vehicleLookup,
+          chain.vehicleLookupSubmitNewBusinessKeeper,
+          chain.businessKeeperDetailsSubmit,
+          chain.newKeeperChooseYourAddressSubmit,
+          chain.taxOrSornVehicle,
+          chain.completeAndConfirmSubmit
+        )
+      )
+  }
+
+  def buyFromTheTraderForPrivate = {
+    val data = csv("data/happy/BuyAVehicleFromTheTraderToPrivate.csv").circular
+    val chain = new Chains(data)
+    scenario("Buy a vehicle from the motor trader as a private keeper from start to finish")
+      .exitBlockOnFail(
+        exec(
+          chain.beforeYouStart,
+          chain.traderDetailsPage,
+          chain.traderDetailsSubmit,
+          chain.businessChooseYourAddress,
+          chain.vehicleLookup,
+          chain.vehicleLookupSubmitNewPrivateKeeper,
+          chain.privateKeeperDetailsSubmit,
+          chain.newKeeperChooseYourAddressSubmit,
+          chain.taxOrSornVehicle,
+          chain.completeAndConfirmSubmit
+        )
+      )
+  }
+
+  def buyFromTraderForPrivateAllOptionalDataFilledIn = {
+    val data = csv("data/happy/BuyAVehicleFromTheTraderToPrivateAllOptionalDataFilledIn.csv").circular
+    val chain = new Chains(data)
+    scenario("Buy a vehicle from the motor trader as a private keeper from start to finish with all optional data filled in")
+      .exitBlockOnFail(
+        exec(
+          chain.beforeYouStart,
+          chain.traderDetailsPage,
+          chain.traderDetailsSubmit,
+          chain.businessChooseYourAddress,
+          chain.vehicleLookup,
+          chain.vehicleLookupSubmitNewPrivateKeeper,
+          chain.privateKeeperDetailsSubmit,
+          chain.newKeeperChooseYourAddressSubmit,
+          chain.taxOrSornVehicle,
+          chain.completeAndConfirmSubmit
+        )
+      )
+  }
+
+  def vehicleLookupUnsuccessful = {
+    val data = csv("data/sad/VehicleLookupUnsuccessful.csv").circular
+    val chain = new Chains(data)
+    scenario("Vehicle lookup is unsuccessful")
+      .exitBlockOnFail(
+        exec(
+          chain.beforeYouStart,
+          chain.traderDetailsPage,
+          chain.traderDetailsSubmit,
+          chain.businessChooseYourAddress,
+          chain.vehicleLookup,
+          chain.vehicleLookupUnsuccessfulSubmit
+        )
+      )
+  }
 }
