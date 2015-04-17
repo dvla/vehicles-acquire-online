1ad1af1f63fb52c1e2b48667a64cdbab3c96481f
Ian Stainer <ian.stainer@valtech.co.uk>
2015-04-17 14:46:14 +0100

    US2889 removed commented out code

--- app/assets/javascripts/acquire-page-init.js
+++ app/assets/javascripts/acquire-page-init.js
@@ -1,7 +1,7 @@
 // Define the dependency to page-init in common
 define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pageInit) {
 
-    var addGoogleAnalyticsEventToTaxLink = function() {
+    var addGaEventToTaxLink = function() {
         //Tracking events for Tax/SORN interactions
         if ($('#tax_details a').length) {
             var taxLink = $('#tax_details a');
@@ -13,36 +13,13 @@ define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pag
     };
 
     var enableSendingGaEventsOnSubmit = function() {
-        // Remove all click handlers and replace with the one below
-        // This is a workaround to having common add a click handler that prevents this one from submitting any page
-        // TODO: Gio to review what's been done here
-//        $('button[type="submit"]').off('click');
         $('button[type="submit"]').on('click', function(e) {
-            console.log("acquire disable submit");
-
             //Tracking events for SORN checkbox submit
             var sornInputChecked = $('#sornVehicle').is(':checked');
             if (sornInputChecked) {
                 _gaq.push(['_setCustomVar', 1, 'taxsorn', 'sorn', 3]);
                 _gaq.push(['_trackEvent', 'taxsorn', 'sorn']);
             }
-/*
-            if ( $(this).hasClass("disabled") ) {
-                return false;
-            }
-
-            $(this).html('Loading').addClass('loading-action disabled');
-            var runTimes = 0;
-            setInterval(function() {
-                if ( runTimes < 3 ){
-                    $('button[type="submit"]').append('.');
-                    runTimes++;
-                } else {
-                    runTimes = 0;
-                    $('button[type="submit"]').html('Loading');
-                }
-            }, 1000);
-*/
         });
     };
 
@@ -50,7 +27,7 @@ define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pag
         init: function() {
             enableSendingGaEventsOnSubmit();
 
-            // Run the common code that is not common to all but this one needs
+            // Run the common code that is not common to all but this exemplar needs
             pageInit.hideEmailOnOther('#privatekeeper_title_titleOption_4', '.form-item #privatekeeper_title_titleText');
             pageInit.hideEmailOnOther('#tax', '#tax_details');
             pageInit.hideEmailOnOther('#sorn', '#sorn_details');
@@ -58,8 +35,7 @@ define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pag
 
             pageInit.imageHintToggles();
 
-            addGoogleAnalyticsEventToTaxLink();
-
+            addGaEventToTaxLink();
 
             // SORN form reset
             $('.sorn-tax-radio-wrapper label input').on('click', function() {
