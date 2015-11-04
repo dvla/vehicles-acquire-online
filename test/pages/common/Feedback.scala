75d1b02ac74efd3675c775c958136f1192deddd2
Oliver Jenkins <github@oliverjenkins.com>
2015-11-04 14:16:37 +0000

    US3955 Set correct value for target attribute on feedback link

--- test/pages/common/Feedback.scala
+++ test/pages/common/Feedback.scala
@@ -1,5 +1,5 @@
 package pages.common
 
 object Feedback {
-final val AcquireEmailFeedbackLink = s"""<a id="${views.common.Feedback.FeedbackId}" href="${controllers.routes.FeedbackController.present()}" target="blank">"""
+final val AcquireEmailFeedbackLink = s"""<a id="${views.common.Feedback.FeedbackId}" href="${controllers.routes.FeedbackController.present()}" target="_blank">"""
 }
