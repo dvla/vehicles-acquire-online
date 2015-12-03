ad4df64116386c7dcab1d8efc49e8bc528d9ee9c
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2015-12-03 10:45:07 +0000

    US3987 remove dependencies on jquery-migrate

--- app/assets/javascripts/acquire-page-init.js
+++ app/assets/javascripts/acquire-page-init.js
@@ -1,5 +1,5 @@
 // Define the dependency to page-init in common
-define(['jquery', 'jquery-migrate', "page-init"], function($, jqueryMigrate, pageInit) {
+define(['jquery', "page-init"], function($, pageInit) {
 
     var addGaEventToTaxLink = function() {
         //Tracking events for Tax/SORN interactions
