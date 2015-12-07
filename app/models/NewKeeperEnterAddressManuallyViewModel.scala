f7dde6e0f36c122a71d3247bc908969203a38b9d
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2015-12-07 08:24:22 +0000

    US4052 PR address picker add three alphas constraint
    Remove unused local NewKeeperEnterAddressManuallyViewModel. Model is defined
    in v-p-c.

--- app/models/NewKeeperEnterAddressManuallyViewModel.scala
+++
@@ -1,8 +0,0 @@
-package models
-
-import play.api.data.Form
-import uk.gov.dvla.vehicles.presentation.common.model.NewKeeperEnterAddressManuallyFormModel
-import uk.gov.dvla.vehicles.presentation.common.model.VehicleAndKeeperDetailsModel
-
-case class NewKeeperEnterAddressManuallyViewModel(form: Form[NewKeeperEnterAddressManuallyFormModel],
-                                                  vehicleAndKeeperDetails: VehicleAndKeeperDetailsModel)
