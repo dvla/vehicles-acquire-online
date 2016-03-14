c96e2af18ec8f933765d6dd5c996a13f2c712cce
Carl Brenton <carl.brenton@dvla.gsi.gov.uk>
2016-03-14 17:20:46 +0000

    US3961 Review commented out / ignored / pending unit tests and main source code

--- test/controllers/TestBruteForcePreventionConfig.scala
+++ test/controllers/TestBruteForcePreventionConfig.scala
@@ -5,13 +5,6 @@ import uk.gov.dvla.vehicles.presentation.common.webserviceclients.bruteforceprev
 
 class TestBruteForcePreventionConfig extends BruteForcePreventionConfig {
 
-  //  val baseUrl: String = getProperty("bruteForcePrevention.baseUrl", "NOT FOUND")
-  //  val requestTimeoutMillis: Int = getProperty("bruteForcePrevention.requestTimeout", 5.seconds.toMillis.toInt)
-  //  val isEnabled: Boolean = getProperty("bruteForcePrevention.enabled", default = true)
-  //  val nameHeader: String = getProperty("bruteForcePrevention.headers.serviceName", "")
-  //  val maxAttemptsHeader: Int = getProperty("bruteForcePrevention.headers.maxAttempts", 3)
-  //  val expiryHeader: String = getProperty("bruteForcePrevention.headers.expiry", "")
-
   override lazy val baseUrl: String =  "NOT FOUND"
   override lazy val requestTimeoutMillis: Int = 5.seconds.toMillis.toInt
   override lazy val isEnabled: Boolean = true
