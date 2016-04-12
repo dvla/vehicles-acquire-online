336114f8bf34b663d0c2a2fb135ba2a3fd2d13ad
Heitham Omar <Heitham.Omar@dvla.gsi.gov.uk>
2016-04-12 09:47:22 +0100

    VMPRCI-2861 Privacy Policy page

---
+++ test/pages/acquire/PrivacyPolicyPage.scala
@@ -0,0 +1,10 @@
+package pages.acquire
+
+import uk.gov.dvla.vehicles.presentation.common.helpers.webbrowser.{Page, WebDriverFactory}
+
+object PrivacyPolicyPage extends Page {
+  def address = s"$applicationContext/privacy-policy"
+
+  override lazy val url = WebDriverFactory.testUrl + address.substring(1)
+  final override val title: String = "Vehicle Management and Personalised Registration Online Privacy Policy"
+}
---
+++ test/views/acquire/PrivacyPolicyUiSpec.scala
@@ -0,0 +1,19 @@
+package views.acquire
+
+import composition.TestHarness
+import helpers.UiSpec
+import org.scalatest.selenium.WebBrowser.{currentUrl, go}
+import pages.acquire.PrivacyPolicyPage
+import uk.gov.dvla.vehicles.presentation.common.testhelpers.UiTag
+
+final class PrivacyPolicyUiSpec extends UiSpec with TestHarness {
+
+  "go to page" should {
+
+    "display the page" taggedAs UiTag in new WebBrowserForSelenium {
+      go to PrivacyPolicyPage
+
+      currentUrl should equal(PrivacyPolicyPage.url)
+    }
+  }
+}
